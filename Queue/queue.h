848af92431b12566eef37d297c6ab70650556077
Mírian <mirianfrsilva@gmail.com>
2018-12-21 00:17:35 -0200

    Create queue.h using linked list

---
+++ Queue/queue.h
@@ -0,0 +1,45 @@
+#include <string>
+// The type of elements contained in the queue is defined by QueueType. The QueueType value must be set at compile time.
+#define QueueType int 
+
+struct Node;
+
+class queue {
+    public:
+  	// Create an empty queue in O(1).
+	queue();
+ 
+	// Releases the allocated memory for all elements of the queue in O(n), where n is the number of elements in the queue.
+	~queue();
+ 
+	// Tests whether the queue is empty in O(1).
+  	bool empty();
+ 
+ 	// Return queue size in O(1).
+	int size();
+ 
+	// Returns the element that is at the beginning of the queue in O(1).
+    // precondition: the queue can not be empty.
+    QueueType front();
+ 
+	// Returns the last element of the queue in O(1).
+	// precondition: the queue can not be empty.
+    QueueType back();
+ 
+	// Insert k at the end of the queue in O(1).
+	void push(QueueType k);
+ 
+	// Remove the element that is at beginning of the queue in O(1).
+	// precondition: the queue can not be empty.
+	void pop();
+ 
+	// Let the current queue be equal to q in O(n + m), where m = q.size () and n is the number of elements in the current queue.
+  	void operator=(queue& q);
+ 
+	private:
+	// Number of elements in the queue.
+	int size_;
+ 
+	// Pointer to the sentinel node of the linked list.
+	Node* end_;
+};
